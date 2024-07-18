202daa09291bdce0321a3e981da65af47de9c23d
okuyama-code <ooyy0121@gmail.com>
2024-07-18 21:56:04 +0900

    修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'airbnb-base',  // baseがないとReactの設定に関するエラーが発生する
     'plugin:@typescript-eslint/recommended',
-    'airbnb-typescript/base',  // baseがないとReactの設定に関するエラーが発生する
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -23,7 +20,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'class-methods-use-this': 'off',  // staticに関するエラーを無視する
-    'import/prefer-default-export': 'off',  // default-exportのエラーを無視する
   },
 };
